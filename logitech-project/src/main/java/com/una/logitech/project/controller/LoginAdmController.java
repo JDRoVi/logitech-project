e231c0738ccee05c6369d6906b1c9d6c6578c4b3
michael <yubank2198@gmail.com>
2020-09-16 10:14:43 -0600

    se añadio user en los archivos de adm, para manejar un usuario y no tener el id como user se soluciono el problema de ingreso a la homepage

---
+++ logitech-project/src/main/java/com/una/logitech/project/controller/LoginAdmController.java
@@ -0,0 +1,51 @@
+
+package com.una.logitech.project.controller;
+
+import com.una.logitech.project.model.administrators.Administrator;
+import java.io.Serializable;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class LoginAdmController implements Serializable{
+    private Administrator Adm;
+    
+    @PostConstruct
+    public void init(){
+        setAdm(new Administrator());
+        
+    }
+    
+    public String login(){
+        FacesContext context=FacesContext.getCurrentInstance();
+        context.getExternalContext().getSessionMap().put("logged",this.getAdm().getId());
+        System.out.println(this.getAdm().getUser());
+        System.out.println(this.getAdm().getPassword());
+        return "/home/Home";
+    }
+    
+    public String logout(){
+        FacesContext context=FacesContext.getCurrentInstance();
+        context.getExternalContext().invalidateSession();
+        return "/login?faces-redirect=true";
+    }
+
+    /**
+     * @return the Adm
+     */
+    public Administrator getAdm() {
+        return Adm;
+    }
+
+    /**
+     * @param Adm the Adm to set
+     */
+    public void setAdm(Administrator Adm) {
+        this.Adm = Adm;
+    }
+    
+    
+}

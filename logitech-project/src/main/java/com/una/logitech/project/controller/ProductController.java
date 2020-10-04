02c41aee734ef5a0d4b34d672eb62823d8814908
Michael ys <55451921+yubank98@users.noreply.github.com>
2020-10-03 22:39:01 -0600

    Create ProductController.java

---
+++ logitech-project/src/main/java/com/una/logitech/project/controller/ProductController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.una.logitech.project.controller;
+
+/**
+ *
+ * @author meys9
+ */
+public class ProductController {
+    
+}

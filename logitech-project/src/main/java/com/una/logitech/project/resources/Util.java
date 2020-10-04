9c7d4ab82b9aa094690507a824573dedba1a6821
Michael ys <55451921+yubank98@users.noreply.github.com>
2020-10-04 15:03:00 -0600

    Update Util.java

--- logitech-project/src/main/java/com/una/logitech/project/resources/Util.java
+++ logitech-project/src/main/java/com/una/logitech/project/resources/Util.java
@@ -1,14 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.una.logitech.project.resources;
 
-/**
- *
- * @author meys9
- */
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
+
+
 public class Util {
-    
+    public static void SaveImgTemporary(byte[] bytes, String name){
+        ServletContext context = (ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext();
+        String path = context.getRealPath("")+File.separatorChar+"resoutces"+File.separatorChar+"images"+File.separatorChar+name;
+        File file=null;
+        InputStream in = null;
+         try{
+            file=new File(path);
+            in=new ByteArrayInputStream(bytes);
+            FileOutputStream out= new FileOutputStream(file.getAbsolutePath());
+            int bit=0;
+            while((bit=in.read())>=0){
+                out.write(bit);
+            }
+            out.flush();
+            out.close();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+    }
 }
